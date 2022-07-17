dd466b6137d28937ffeae18fe52fb10bdc038e18
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-18 01:11:19 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 1? <p>{props.bio}</p> : ""}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links linkedin={props.links.linkedin} github={props.links.github} />
       {/* add your <Links /> component here */}
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,13 @@ console.log(user);
 function App() {
   return (
     <div>
+      
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
 
 export default App;
+
